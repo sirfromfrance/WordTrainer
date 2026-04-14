d05eb16c4ed623e9396415000b814f08184d6a5e
Valeriia <gritsenko5555@gmail.com>
2026-04-14 23:49:54 +0300

    updated card DAO

--- app/src/main/java/com/example/wordtrainer/data/data_source/CardDao.kt
+++ app/src/main/java/com/example/wordtrainer/data/data_source/CardDao.kt
@@ -1,10 +1,17 @@
 package com.example.wordtrainer.data.data_source
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
 
 
 @Dao
 interface CardDao{
+    @Query("SELECT * FROM Card WHERE wordId = :id")
+    suspend fun getCardById(id:Int): Card?
+
+    @Delete
+    suspend fun deleteCard(card: Card)
+
 
 }
--- app/src/main/java/com/example/wordtrainer/data/repository/CardRepositoryImpl.kt
+++ app/src/main/java/com/example/wordtrainer/data/repository/CardRepositoryImpl.kt
@@ -1,9 +1,30 @@
 package com.example.wordtrainer.data.repository
+import com.example.wordtrainer.data.data_source.Card
 import com.example.wordtrainer.data.data_source.CardDao
 import com.example.wordtrainer.domain.repository.CardRepository
+import kotlinx.coroutines.flow.Flow
 
 class CardRepositoryImpl (
     private val dao: CardDao
 ): CardRepository {
+    override fun getCards(): Flow<List<Card>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCard(id: Int): Card? {
+        return dao.getCardById(id)
+    }
+
+    override suspend fun insertCard(card: Card) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateCard(card: Card) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCard(card: Card) {
+        return dao.deleteCard(card)
+    }
 
 }
